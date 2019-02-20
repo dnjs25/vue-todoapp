3814a95c14aa1deaac3dc9d4e26f0cbdabb23fb5
yewon <won29255@naver.com>
2019-02-20 16:46:47 +0900

    todo app

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
+    <!-- v-on:하위 컴포넌트에서 발생시킨 이벤트 이름="현재 컴포넌트에서 발생시킨 메소드명" -->
+    <TodoList v-bind:propsdata="todoItems"
+        v-on:removeItem="removeOneItem"
+        v-on:toggleItem="toggleOneItem">
+    </TodoList>
+    <!-- v-bind:내려보낼 프롭스 속성 이름 = "현재 위치의 컴포넌트 데이터 속성" -->
+    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    addOneItem: function(todoItem) {
+      var obj = {completed: false, item: todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
+    },
+    removeOneItem: function(todoItem, index) {
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);
+    },
+    toggleOneItem: function(todoItem, index) {
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItems: function() {
+      localStorage.clear();
+      this.todoItems = [];
+    }
+  },
+  created: function() {
+    // 인스턴스가 생성 되자마자 호출되는 라이프사이클 훅
+    // 생성되는 시점에 created의 logic이 호출됨
+    if (localStorage.length > 0) {
+      for (var i=0; i<localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+  },
+  components: {
+    // 컴포넌트 이름: 컴포넌트 내용
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter
+  }
+}
+</script>
+
+<style>
+  body {
+    text-align: center;
+    background-color: #f6f6f6;
+  }
+
+  #app {
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+
+  button {
+    border-style: groove;
+  }
+
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  }
+
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <!-- v-model: input에 입력된 값을 동적으로 뷰 인스턴스 안에 맵핑 -->
+        <!-- <button v-on:click="addTodo" class="addBtn">add</button> -->
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
+
+        <Modal v-if="showModal" @close="showModal = false">
+            <!-- slot은 상위 컴포넌트에서 재정의해서 사용이 가능함 -->
+            <h3 slot="header">
+                경고!
+                <i class="fas fa-times closeModalBtn" @click="closeModalBtn"></i>
+            </h3>
+
+            <h4 slot="body">내용을 입력하세요.</h4>
+
+            <!-- <h5 slot="footer"></h5> -->
+        </Modal>
+    </div>
+</template>
+
+<script>
+import Modal from './common/Modal.vue'
+
+export default {
+    data: function() {
+        return {
+            newTodoItem: "",
+            showModal: false
+        }
+    },
+    methods: {
+        addTodo: function() {
+            if (this.newTodoItem !== '') {
+            // var obj = {completed: false, item: this.newTodoItem};
+            // localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+            // localStorage.setItem(this.newTodoItem, obj);
+            // obj를 그냥 넣을경우 값이 확인이 안됨.
+            //this <- 같은 인스턴스 접근 가능
+            this.$emit('addTodoItem', this.newTodoItem);
+            this.clearInput();
+            } else {
+                this.showModal = !this.showModal;
+            }
+        },
+        clearInput: function() {
+            console.log("clearInput");
+            this.newTodoItem = '';
+        },
+        closeModalBtn: function() {
+            this.showModal = !this.showModal;
+        }
+    },
+    components: {
+        'Modal': Modal,
+    }
+}
+</script>
+
+<style scoped>
+    input:focus {
+        outline: none;
+    }
+    .inputBox {
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input {
+        border-style: none;
+        font-size: 0.9rem;
+    }
+    .addContainer {
+        float: right;
+        background: linear-gradient(to right, #6478FB, #8763FB);
+        display: block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addContainer:hover {
+        cursor: pointer;
+    }
+    .addBtn {
+        color: white;
+        vertical-align: middle;
+    }
+    .closeModalBtn {
+        color: #42b983;
+    }
+    .closeModalBtn:hover {
+        cursor: pointer;
+    }
+</style>
