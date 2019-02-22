df8118e87e05ad6c362abceb87c8c8d76b10bd15
yewon <won29255@naver.com>
2019-02-22 17:31:05 +0900

    Setting vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { store } from './store/store'
 
 new Vue({
   el: '#app',
+  // 축약 가능
+  store,
   render: h => h(App)
 })
---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// vue 플러그인 사용
+// vue를 사용하는 모든 영역에 (전역으로) 특정 기능을 추가하고 싶을 때
+// global functionality를 사용하고 싶을 때 사용
+// 특정 컴포넌트에서 접근이 가능
+
+Vue.use(Vuex);
+
+// 다른 파일에서 접근 가능
+export const store = Vuex.Store({
+    // 
+});
