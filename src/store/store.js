2c312c259d84e2f6a856187673ed8cf800effe3e
yewon <won29255@naver.com>
2019-02-25 18:38:50 +0900

    vuex-modules

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as getters from './getters'
+import * as mutations from './mutations'
+
 
 // vue 플러그인 사용
 // vue를 사용하는 모든 영역에 (전역으로) 특정 기능을 추가하고 싶을 때
@@ -30,29 +33,6 @@ export const store = new Vuex.Store({
     state: {
         todoItems: storage.fetch()
     },
-    getters: {
-        storedTodoItems(state) {
-            return state.todoItems;
-        }
-    },
-    mutations: {
-        addOneItem(state, todoItem) {
-            const obj = { completed: false, item: todoItem };
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-        removeOneItem(state, payload) {
-            localStorage.removeItem(payload.todoItem.item);
-            state.todoItems.splice(payload.index, 1);
-        },
-        toggleOneItem(state, payload) {
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-            localStorage.removeItem(payload.todoItem.item);
-            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-        },
-        clearAllItems(state) {
-            localStorage.clear();
-            state.todoItems = [];
-        },
-    }
+    getters,
+    mutations
 });
